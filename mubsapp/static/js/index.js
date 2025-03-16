fbfce54e3f39fda6c0e5051922054d1dce859609
kalosius <kalosius43@gmail.com>
2025-03-16 19:17:27 +0300

    fixed nav

--- mubsapp/static/js/index.js
+++ mubsapp/static/js/index.js
@@ -11,8 +11,12 @@ document.getElementById('close-message').onclick = function() {
 // side navigation on small devices
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-body").style.backgroundColor = "rgba(0,0,0,0.4)"; // Dim background
+
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-body").style.backgroundColor = ""; // Reset background
+
 }
