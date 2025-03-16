93df50464afe47b5bda58d2c64f0615d0427dc04
kalosius <kalosius43@gmail.com>
2025-03-16 14:17:55 +0300

    hosts

--- brainytoes/settings.py
+++ brainytoes/settings.py
@@ -1,6 +1,9 @@
-
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +18,7 @@ SECRET_KEY = 'django-insecure-cpbv(i(w$hu*w8p)vh70t0e&vgnt_c+ni&y2q67#lh88w70^%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://mubsapp.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -66,8 +69,12 @@ WSGI_APPLICATION = 'brainytoes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'your_db_name'),
+        'USER': os.getenv('DB_USER', 'your_db_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_db_password'),
+        'HOST': os.getenv('DB_HOST', 'your_db_host'),
+        'PORT': os.getenv('DB_PORT', 'your_db_port'),
     }
 }
 
