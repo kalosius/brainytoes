de0c5fe915e7e5c122fe80d71c48d9e70bbcc985
kalosius <kalosius43@gmail.com>
2025-03-17 15:47:21 +0300

    title in django

--- mubsapp/admin.py
+++ mubsapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Book, Software, SoftwareCategory, Tutorial
 
+admin.site.site_header = "BrainyToes Admin"
+admin.site.site_title = "BrainyToes Admin Portal"
+admin.site.index_title = "Welcome to BrainyToes Admin"
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'pdf')
 
