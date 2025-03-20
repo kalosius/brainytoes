42acc7d3b9b7a9154e2e1e176672dcbd2310f690
kalosius <kalosius43@gmail.com>
2025-03-20 03:51:45 +0300

    removed unecessary field

--- mubsapp/admin.py
+++ mubsapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book, Software, SoftwareCategory, Tutorial
+from .models import Book, Software, SoftwareCategory, Tutorial,Profile
+from django.contrib.auth.models import User
 
 admin.site.site_header = "BrainyToes Admin"
 admin.site.site_title = "BrainyToes Admin Portal"
@@ -12,3 +13,22 @@ admin.site.register(Book, BookAdmin)
 admin.site.register(Software)
 admin.site.register(Tutorial)
 admin.site.register(SoftwareCategory)
+admin.site.register(Profile)
+
+
+
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	field = ["username", "first_name", "last_name", "email"]
+	inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
--- mubsapp/models.py
+++ mubsapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -11,6 +14,7 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/')
     language = models.CharField(max_length=50)
     pdf = models.FileField(upload_to='books/pdfs/', null=True, blank=True)
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.title
@@ -33,6 +37,7 @@ class Software(models.Model):
     category = models.ForeignKey(SoftwareCategory, on_delete=models.CASCADE)
     size = models.FloatField(default=0.0)
     description = models.TextField(max_length=500, null=True, blank=True)
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
@@ -45,6 +50,32 @@ class Tutorial(models.Model):
     duration = models.DurationField()
     youtube_link = models.URLField(blank=True, null=True)
     file = models.FileField(upload_to='tutorial_files/', blank=True, null=True)
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.title
+
+
+# Create Customer Profile
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_modified = models.DateTimeField(User, auto_now=True)
+	phone = models.CharField(max_length=20, blank=True)
+	address1 = models.CharField(max_length=200, blank=True)
+	address2 = models.CharField(max_length=200, blank=True)
+	city = models.CharField(max_length=200, blank=True)
+	state = models.CharField(max_length=200, blank=True)
+	zipcode = models.CharField(max_length=200, blank=True)
+	country = models.CharField(max_length=200, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+# Create a user Profile by default when user signs up
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+
+# Automate the profile thing
+post_save.connect(create_profile, sender=User)
