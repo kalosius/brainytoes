c1c29fc4fb3d604538737deac0f84aec6dfa1921
kalosius <kalosius43@gmail.com>
2025-04-01 21:37:06 +0300

    Unecessary code

--- brainytoes/settings.py
+++ brainytoes/settings.py
@@ -155,7 +155,14 @@ LOGOUT_REDIRECT_URL = '/'
 # Caching
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1'),
+        # 'OPTIONS': {
+        #     'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        # },
     }
 }
+
+# Optional: Use Redis for session storage
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_CACHE_ALIAS = 'default'
